0c06829d32cf09a52b6e44a710b58a7ed9310ac9
azamjonbro <azamjondasturchi11@gmail.com>
2025-12-23 19:14:45 +0500

    added

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -5,7 +5,7 @@ const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "NAX API",
+      title: "Happy Pet API Documentation",
       version: "1.0.0"
     },
     servers: [
